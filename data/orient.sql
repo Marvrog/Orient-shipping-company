378b174e4b94d60e15ee275c4f08a33806466260
Marvrog <susuasseymarvelousguy@gmail.com>
2021-12-05 13:53:27 +0000

    chore: create packageTrack table

--- data/orient.sql
+++ data/orient.sql
@@ -5,7 +5,7 @@ CREATE DATABASE Orient;
 
 --  User Account
 CREATE TABLE Users(
-    id int not null auto_increment,
+    userID int not null auto_increment,
     username varchar(50) not null,
     secret_code varchar(50) not null,
     primary key(id)
@@ -13,7 +13,7 @@ CREATE TABLE Users(
 
 -- Package
 CREATE TABLE Package(
-    id int not null auto_increment,
+    packageID int not null auto_increment,
     waybill_id int not null,
     sender varchar(255) not null,
     receiver varchar(255) not null,
@@ -24,4 +24,13 @@ CREATE TABLE Package(
     package_trackID int not null,
     primary key(id),
     foreign key(package_trackID) references PackageTrack(package_trackID)
+);
+
+-- Package Track
+CREATE TABLE PackageTrack(
+    package_trackID int not null auto_increment,
+    location varchar(255) not null,
+    status varchar(50) not null,
+    track_date datetime,
+    primary key(package_trackID)
 );
